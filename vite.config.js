3fd8707345ce3fdcddaecb7d5254a36905d97970
Kayro <kayro2007@qq.com>
2026-02-16 02:51:35 +0800

    构建优化

--- vite.config.js
+++ vite.config.js
@@ -88,9 +88,21 @@ export default ({ mode }) =>
     },
     build: {
       minify: "terser",
+      chunkSizeWarningLimit: 600,
       terserOptions: {
         compress: {
           pure_funcs: ["console.log"],
+        },
+      },
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            "vue-vendor": ["vue", "pinia"],
+            "element-plus": ["element-plus"],
+            "swiper": ["swiper"],
+            "aplayer": ["aplayer", "@worstone/vue-aplayer"],
+            "icons": ["@fortawesome/fontawesome-svg-core", "@fortawesome/free-solid-svg-icons", "@fortawesome/vue-fontawesome", "@icon-park/vue-next"],
+          },
         },
       },
     },
