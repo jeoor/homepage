2fa2cebbd6691b2221195312e847c7704811abf1
Kayro <kayro2007@qq.com>
2026-03-05 01:08:57 +0800

    站点更新+开启右键

--- src/App.vue
+++ src/App.vue
@@ -80,15 +80,15 @@ onMounted(() => {
   // 自定义鼠标
   cursorInit();
 
-  // 屏蔽右键
-  document.oncontextmenu = () => {
-    ElMessage({
-      message: "为了浏览体验，本站禁用右键",
-      grouping: true,
-      duration: 2000,
-    });
-    return false;
-  };
+  // // 屏蔽右键
+  // document.oncontextmenu = () => {
+  //   ElMessage({
+  //     message: "为了浏览体验，本站禁用右键",
+  //     grouping: true,
+  //     duration: 2000,
+  //   });
+  //   return false;
+  // };
 
   // 鼠标中键事件
   window.addEventListener("mousedown", (event) => {
